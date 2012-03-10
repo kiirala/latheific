d5f1918c9f4db1e2d6027ec119dcf77301cdc63a
Niko Kiirala <niko@kiirala.com>
2012-03-10 11:29:19 +0200

    Randomness on screen

--- scene.c
+++ scene.c
@@ -41,6 +41,32 @@ static void setupProjection() {
   glLoadIdentity();
 }
 
+static void drawAxis() {
+  glShadeModel(GL_FLAT);
+  glDisable(GL_DEPTH_TEST);
+  glDisable(GL_TEXTURE_2D);
+  //glUseProgram(0);
+
+  glBegin (GL_LINES);
+  glColor3f (1., 0., 0.);
+  glVertex3f (0., 0., 0.);
+  glVertex3f (1., 0., 0.);
+  glEnd ();
+	
+  glBegin (GL_LINES);
+  glColor3f (0., 1., 0.);
+  glVertex3f (0., 0., 0.);
+  glVertex3f (0., 1., 0.);
+  glEnd ();
+	
+  glBegin (GL_LINES);
+  glColor3f (0., 0., 1.);
+  glVertex3f (0., 0., 0.);
+  glVertex3f (0., 0., 1.);
+  glEnd ();
+
+}
+
 static Mesh* lathe(float *points, int pointCount, int steps) {
   int vertexCount = (pointCount * 2 + 2) * steps - 2;
   Mesh* mesh = malloc(sizeof(Mesh));
@@ -81,24 +107,43 @@ static Mesh* lathe(float *points, int pointCount, int steps) {
   return mesh;
 }
 
+static void ballVertexMap(Mesh *mesh) {
+  for (int i = 0 ; i < mesh->count ; ++i) {
+    float x = mesh->loc[i * 3];
+    float y = mesh->loc[i * 3 + 1];
+    float z = mesh->loc[i * 3 + 2];
+    float len = sqrt(x*x + y*y + z*z);
+    mesh->normal[i * 3] = x / len;
+    mesh->normal[i * 3 + 1] = y / len;
+    mesh->normal[i * 3 + 2] = y / len;
+  }
+}
+
 Mesh* someMesh;
 Object* someObject;
 
 void load() {
   float points[] = {0, -1, 1, -.2, .8, .2, 0, 1};
   someMesh = lathe(points, 4, 20);
+  ballVertexMap(someMesh);
+
   someObject = malloc(sizeof(Object));
   someObject->mesh = someMesh;
+
   glGenVertexArrays(1, &someObject->vao);
   glBindVertexArray(someObject->vao);
   someObject->vbo = malloc(sizeof(GLuint) * 2);
-  glGenBuffers(2, someObject->vbo);
+
+  glGenBuffers(3, someObject->vbo);
   glBindBuffer(GL_ARRAY_BUFFER, someObject->vbo[0]);
   glBufferData(GL_ARRAY_BUFFER, someMesh->count * 3 * sizeof(GLfloat),
 	       someMesh->loc, GL_STATIC_DRAW);
   glBindBuffer(GL_ARRAY_BUFFER, someObject->vbo[1]);
   glBufferData(GL_ARRAY_BUFFER, someMesh->count * 3 * sizeof(GLfloat),
 	       someMesh->colour, GL_STATIC_DRAW);
+  glBindBuffer(GL_ARRAY_BUFFER, someObject->vbo[2]);
+  glBufferData(GL_ARRAY_BUFFER, someMesh->count * 3 * sizeof(GLfloat),
+	       someMesh->normal, GL_STATIC_DRAW);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindVertexArray(0);
   logGL();
@@ -112,48 +157,43 @@ void render(int time) {
 
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  glTranslatef(0, 0, -2);
-  glRotatef (time / 1000.0f * 360.0f, 1, 0, 1);
-  //glScalef(0.3, 0.3, 0.3);
-  // glRotatef (ang, 0, 1, 0);
-  // glRotatef (ang, 0, 0, 1);
-
-  glShadeModel(GL_FLAT);
-  glDisable(GL_DEPTH_TEST);
-  glDisable(GL_TEXTURE_2D);
-  //glUseProgram(0);
-
-  glBegin (GL_LINES);
-  glColor3f (1., 0., 0.);
-  glVertex3f (0., 0., 0.);
-  glVertex3f (1., 0., 0.);
-  glEnd ();
-	
-  glBegin (GL_LINES);
-  glColor3f (0., 1., 0.);
-  glVertex3f (0., 0., 0.);
-  glVertex3f (0., 1., 0.);
-  glEnd ();
-	
-  glBegin (GL_LINES);
-  glColor3f (0., 0., 1.);
-  glVertex3f (0., 0., 0.);
-  glVertex3f (0., 0., 1.);
-  glEnd ();
-
-  glScalef(2, 2, 2);
-  //glBindVertexArray(someObject->vao);
-  logGL();
-  glBindVertexArray(0);
-  glEnableClientState(GL_VERTEX_ARRAY);
-  glBindBuffer(GL_ARRAY_BUFFER, someObject->vbo[0]);
-  glVertexPointer(3, GL_FLOAT, 0, 0);
-  glBindBuffer(GL_ARRAY_BUFFER, someObject->vbo[1]);
-  glColorPointer(3, GL_FLOAT, 0, 0);
-  logGL();
-  glDrawArrays(GL_TRIANGLE_STRIP, 0, someObject->mesh->count);
-  glDisableClientState(GL_VERTEX_ARRAY);
-  logGL();
+  glShadeModel(GL_SMOOTH);
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_LIGHTING);
+  glEnable(GL_LIGHT0);
+      
+  int xcount = 16;
+  int ycount = 12;
+  for (int x = 0 ; x < xcount ; ++x) {
+    for (int y = 0 ; y < ycount ; ++y) {
+      glLoadIdentity();
+      glRotatef(sin(time / 1000.0f), 0, 0, 1);
+      glTranslatef((x - xcount / 2 + sin(time / 1000.0f * 39.0f)) * 3.5,
+		   (y - ycount / 2 + sin(time / 1000.0f * 239.0f)) * 3.5,
+		   -2);
+      glRotatef ((time / 1000.0f + (x ^ y) / 16.0f) * 360.0f, 1, 0, 1);
+      //glScalef(0.3, 0.3, 0.3);
+      // glRotatef (ang, 0, 1, 0);
+      // glRotatef (ang, 0, 0, 1);
+      
+      //glScalef(2, 2, 2);
+      //glBindVertexArray(someObject->vao);
+      logGL();
+      glBindVertexArray(0);
+      glEnableClientState(GL_VERTEX_ARRAY);
+      glBindBuffer(GL_ARRAY_BUFFER, someObject->vbo[0]);
+      glVertexPointer(3, GL_FLOAT, 0, 0);
+      glBindBuffer(GL_ARRAY_BUFFER, someObject->vbo[1]);
+      glColorPointer(3, GL_FLOAT, 0, 0);
+      glBindBuffer(GL_ARRAY_BUFFER, someObject->vbo[2]);
+      glNormalPointer(GL_FLOAT, 0, 0);
+      logGL();
+      glDrawArrays(GL_TRIANGLE_STRIP, 0, someObject->mesh->count);
+      glDisableClientState(GL_VERTEX_ARRAY);
+      logGL();
+    }
+  }
+  //drawAxis();
 }
 
 void resize(int width, int height) {
